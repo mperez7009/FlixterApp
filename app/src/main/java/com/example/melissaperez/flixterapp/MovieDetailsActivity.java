ce1a33f9e3327d268b19cc1897e2c09d76b51876
Melissa Perez <melissaperez@fb.com>
2017-06-23 16:38:32 -0700

    Movies playing now

---
+++ app/src/main/java/com/example/melissaperez/flixterapp/MovieDetailsActivity.java
@@ -0,0 +1,54 @@
+package com.example.melissaperez.flixterapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.melissaperez.flixterapp.models.Movie;
+
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+
+    // the movie to display
+    Movie movie;
+
+    // the view objects
+    @BindView(R.id.tvTitle) TextView tvTitle;
+    @BindView(R.id.tvOverview) TextView tvOverview;
+    @BindView(R.id.rbVoteAverage) RatingBar rbVoteAverage;
+    //TextView tvTitle;
+    //TextView tvOverview;
+    //RatingBar rbVoteAverage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        ButterKnife.bind(this);
+        // resolve the view objects
+        //tvTitle = (TextView) findViewById(R.id.tvTitle);
+        //tvOverview = (TextView) findViewById(R.id.tvOverview);
+        //rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        // unwrap the movie passed in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+    }
+}
---
+++ app/src/main/java/com/example/melissaperez/flixterapp/models/Movie.java
@@ -0,0 +1,52 @@
+package com.example.melissaperez.flixterapp.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+/**
+ * Created by melissaperez on 6/22/17.
+ */
+
+@Parcel
+public class Movie {
+
+    // values from API
+    String title;
+    String overview;
+    String posterPath; // only the path
+    String backdropPath;
+    Double voteAverage;
+
+    public Movie() {}
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    //initialize from JSON data
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+}
